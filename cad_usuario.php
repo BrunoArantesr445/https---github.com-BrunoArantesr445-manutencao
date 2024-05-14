28529dba65549886a3e7fee856b2f2102fb13ef2
bruno <brunogon542@gmail.com>
2024-05-14 08:19:13 -0300

    lalarara

--- cad_usuario.php
+++ cad_usuario.php
@@ -6,61 +6,61 @@ $db = new banco;
  
  
  echo "<pre>";
- print_r($_SESSION);
+ print_r($_POST);
  echo "</pre>";
  
-if (isset($_SESSION['username'])) {
+// if (isset($_SESSION['username'])) {
  
  
  
-if (isset($_POST['salva_usuario'])) {
-    $inputData = [
-        'nome' => mysqli_real_escape_string($db->conn, $_POST['nome']),
-        'login' => mysqli_real_escape_string($db->conn, $_POST['login']),
-        'senha' => mysqli_real_escape_string($db->conn, $_POST['senha']),
-        'confirma_senha' => mysqli_real_escape_string($db->conn, $_POST['senha_confirma']),
-    ];
+// if (isset($_POST['salva_usuario'])) {
+//     $inputData = [
+//         'nome' => mysqli_real_escape_string($db->conn, $_POST['nome']),
+//         'login' => mysqli_real_escape_string($db->conn, $_POST['login']),
+//         'senha' => mysqli_real_escape_string($db->conn, $_POST['senha']),
+//         'confirma_senha' => mysqli_real_escape_string($db->conn, $_POST['senha_confirma']),
+//     ];
  
  
  
-    $senha = $_POST['senha'];
-    $senha_confirma = $_POST['senha_confirma'];
+//     $senha = $_POST['senha'];
+//     $senha_confirma = $_POST['senha_confirma'];
  
-    if ($senha <> $senha_confirma) {
-        echo "Senhas diferentes";
-        header("location: cadastro_usuario.php");
-    } else  if (($senha == $senha_confirma) and ($senha != "")) {
-        echo "tudo certo";
-        $usuario = new usuario;
-        $result = $usuario->criar($inputData);
-    } else if ($senha == " ") {
-        echo "ta vazio ze";
-        header("location: cadastro_usuario.php");
-        return false;
-    } else {
-        echo "para de ser burro'";
-        header("location: cadastro_usuario.php");
-        return false;
-    }
+//     if ($senha <> $senha_confirma) {
+//         echo "Senhas diferentes";
+//         header("location: cadastro_usuario.php");
+//     } else  if (($senha == $senha_confirma) and ($senha != "")) {
+//         echo "tudo certo";
+//         $usuario = new usuario;
+//         $result = $usuario->criar($inputData);
+//     } else if ($senha == " ") {
+//         echo "ta vazio ze";
+//         header("location: cadastro_usuario.php");
+//         return false;
+//     } else {
+//         echo "para de ser burro'";
+//         header("location: cadastro_usuario.php");
+//         return false;
+//     }
  
-    //print_r($result);
+//     //print_r($result);
  
  
-    if ($result) {
-        $_SESSION['message'] = "Cadastro ok - bem vindo " . $_POST['nome'];
-        header("location: login_sup.php");
-        exit(0);
-    } else {
-        $_SESSION['message'] = "errou BB";
-        header("location: cadastro_usuario.php");
-        exit(0);
-    }
-} else {
-    $_SESSION['message'] = "Tu ta tentando acessar direto =)";
-    header("location: cadastro_usuario.php");
-    exit(0);
-}
-}else{
-  header("location: index.php");
-   echo "marmota";
-}
+//     if ($result) {
+//         $_SESSION['message'] = "Cadastro ok - bem vindo " . $_POST['nome'];
+//         header("location: login_sup.php");
+//         exit(0);
+//     } else {
+//         $_SESSION['message'] = "errou BB";
+//         header("location: cadastro_usuario.php");
+//         exit(0);
+//     }
+// } else {
+//     $_SESSION['message'] = "Tu ta tentando acessar direto =)";
+//     header("location: cadastro_usuario.php");
+//     exit(0);
+// }
+// }else{
+//  header("location: index.php");
+
+// }
